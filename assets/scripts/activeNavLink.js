8dcf78db9619d6c9e7dc2c9554f1bd0848685d04
Yyertsenoj94 <harvjones1994@gmail.com>
2022-10-16 16:42:44 -0500

    removing old files

--- assets/scripts/activeNavLink.js
+++
@@ -1,9 +0,0 @@
-//makes the link to the current url in an "active" state on the header
-$(document).ready(() =>{ 
-    //get the file link of the page
-    var url = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
-
-    //selects the html a element with attribute href = url
-    var target = $('a[href="' + url + '"]');
-    target.addClass('custom-active'); //custom css class to modify the background color of the active item
-})
