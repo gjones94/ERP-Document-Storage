d5ac56929590f81b301358ce4bdf02c785ee09ed
Yyertsenoj94 <harvjones1994@gmail.com>
2022-10-30 05:34:32 +0000

    added loggin

--- cronjobs/cleanup_upload_files.sh
+++ cronjobs/cleanup_upload_files.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-path="/var/www/html/doc_storage/uploads"
-files=$( ls $path )
-for file in $files; do
-    file=$path/$file
-    file_create=`date -r $file +%s`;
-    date_now=`date +%s`;
-    difference=$(( ($date_now - $file_create) / 60))
-    if [[ $difference -gt 5 ]]; then
-        echo "$file was created more than 5 minutes ago";
-        echo "Deleting $file";
-        rm $file    
-    fi
+echo "================Running Cron job================"
+echo "==========$(date)=========="
 
-done
+path="/var/www/html/doc_storage/uploads"
+
+files=($(ls $path))
+
+num_files=${#files[@]}
+
+if [[ $num_files == 0 ]];then
+    echo "No files to delete"
+else
+    for file in $files; do
+        file=$path/$file
+        file_create=`date -r $file +%s`;
+        date_now=`date +%s`;
+        difference=$(( ($date_now - $file_create) / 60))
+        if [[ $difference -gt 1 ]]; then
+            echo "$file was created more than 5 minutes ago";
+            echo "Deleting $file";
+            rm $file    
+        else
+            echo $file not old enough to delete
+        fi
+    done
+fi
+echo "==================End Cron Job=================="
